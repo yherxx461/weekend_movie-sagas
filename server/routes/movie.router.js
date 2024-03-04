c37342c02afc855fabe12b8a777583b1892a69f8
Ying Her <yher.prime@gmail.com>
2024-03-03 23:37:49 -0600

    created a new GET router to get specific movie using params and id

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
+const pool = require('../modules/pool');
 
+// Get all movie data from movies
 router.get('/', (req, res) => {
   const query = `
     SELECT * FROM "movies"
       ORDER BY "title" ASC;
   `;
-  pool.query(query)
-    .then(result => {
+  pool
+    .query(query)
+    .then((result) => {
       res.send(result.rows);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log('ERROR: Get all movies', err);
-      res.sendStatus(500)
-    })
+      res.sendStatus(500);
+    });
+});
 
+// Get individual movie from movies with genre data from movies_genres.genre_id (MANY-TO-MANY RELATIONSHIP)
+router.get('/', (req, res) => {
+  const id = req.params.id;
+  const query = `SELECT movies.id AS id, movies.title AS title, movies.poster AS poster, movies.description AS description,
+  STRING_AGG(genres.name) AS genres FROM movies
+  JOIN movies_genres ON movies_genres.movie_id = movies.id
+  JOIN genres ON genres.id = movies_genres.genre_id
+  WHERE movies.id = $1
+  GROUP BY movies.id
+  ORDER BY movies.id;`;
+
+  pool
+    .query(query, [id])
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch((error) => {
+      console.error('Error in getting movie result', error);
+      res.sendStatus(500);
+    });
 });
 
 router.post('/', (req, res) => {
@@ -31,14 +54,15 @@ router.post('/', (req, res) => {
   const insertMovieValues = [
     req.body.title,
     req.body.poster,
-    req.body.description
-  ]
+    req.body.description,
+  ];
   // FIRST QUERY MAKES MOVIE
-  pool.query(insertMovieQuery, insertMovieValues)
-    .then(result => {
+  pool
+    .query(insertMovieQuery, insertMovieValues)
+    .then((result) => {
       // ID IS HERE!
       console.log('New Movie Id:', result.rows[0].id);
-      const createdMovieId = result.rows[0].id
+      const createdMovieId = result.rows[0].id;
 
       // Now handle the genre reference:
       const insertMovieGenreQuery = `
@@ -47,24 +71,25 @@ router.post('/', (req, res) => {
           VALUES
           ($1, $2);
       `;
-      const insertMovieGenreValues = [
-        createdMovieId,
-        req.body.genre_id
-      ]
+      const insertMovieGenreValues = [createdMovieId, req.body.genre_id];
       // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
-      pool.query(insertMovieGenreQuery, insertMovieGenreValues)
-        .then(result => {
+      pool
+        .query(insertMovieGenreQuery, insertMovieGenreValues)
+        .then((result) => {
           //Now that both are done, send back success!
           res.sendStatus(201);
-        }).catch(err => {
+        })
+        .catch((err) => {
           // catch for second query
           console.log(err);
-          res.sendStatus(500)
-      })
-    }).catch(err => { // 👈 Catch for first query
-      console.log(err);
-      res.sendStatus(500)
+          res.sendStatus(500);
+        });
     })
-})
+    .catch((err) => {
+      // 👈 Catch for first query
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
