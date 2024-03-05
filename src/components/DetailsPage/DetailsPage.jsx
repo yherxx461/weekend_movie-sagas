1ea131c31e32b7c3c2c33922699b5bbb0f3e9ca2
Ying Her <yher.prime@gmail.com>
2024-03-05 00:05:48 -0600

    swtiched back to filtering/finding the specific id and not using dispatch as it still have issues rendering genre

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,37 +1,20 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 
 function DetailsPage() {
   // Calling id from params
-  const details = useSelector((store) => store.movieDetails);
+  const movies = useSelector((store) => store.movies);
   const { id } = useParams();
-  // const [movieData, setMovieData] = useState();
+  const [movieData, setMovieData] = useState();
   const history = useHistory();
-  const dispatch = useDispatch();
-  const fetchMovieDetails = (action) => {
-    dispatch({ type: 'FETCH_MOVIE_DETAILS' });
-    axios
-      .get(`api/movies/${id}`)
-      .then((response) => {
-        // dispatch({
-        //   type: 'SET_MOVIE_DETAILS',
-        //   payload: action,
-        // });
-      })
-      .catch((error) => {
-        console.log('ERROR', error);
-      });
-  };
 
   useEffect(() => {
-    fetchMovieDetails();
-    // array.find() method helps find the movie object by directly looking for the specific id
-    // const movie = details.find((movieItem) => movieItem.id == parseInt(id));
-    // setMovieData(movie);
-  }, [id]);
+    //array.find() method helps find the movie object by directly looking for the specific id;
+    const movie = movies.find((movieItem) => movieItem.id == parseInt(id));
+    setMovieData(movie);
+  }, [movies, id]);
 
   const handleBackToMovies = () => {
     console.log('in handleBackToMovies');
@@ -42,13 +25,13 @@ function DetailsPage() {
       {/* TO-DO: "movieDetails attribute added." */}
       <div data-testid="movieDetails" className="movieDetails">
         <h1>Movie Details</h1>
-        {details && (
+        {movieData && (
           <div>
-            <h3>{details.title}</h3>
-            <img src={details.poster} alt={details.title} />
-            <p>Description: {details.description}</p>
+            <h3>{movieData.title}</h3>
+            <img src={movieData.poster} alt={movieData.title} />
+            <p>Description: {movieData.description}</p>
             {/* TO-DO: make genre appears here. */}
-            <p>Genre: {details.genre_names}</p>
+            <p>Genre: {movieData.genre_names}</p>
           </div>
         )}
         {/* TO-DO: Must have a "back to movie list" button with data-testid="toList" attributes */}
